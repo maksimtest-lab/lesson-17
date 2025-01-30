911915edcf90b3434ecdbebc99121275844b3975
Maksim Alipau <maxanotest@gmail.com>
2025-01-30 22:50:49 +0300

    feat: add a task 11

--- hw/task11.js
+++ hw/task11.js
@@ -1,1 +1,18 @@
 // Программа загадывает случайное число от 1 до 100. Пользователь должен угадать число, вводя свои варианты. Программа подсказывает: "меньше" или "больше".
+
+const randomNumber = Math.floor(Math.random() * 100);
+let userGuess;
+
+do {
+    userGuess = +prompt('Угадайте число от 1 до 100:');
+
+    if (userGuess < randomNumber) {
+        console.log('Больше');
+    } else if (userGuess > randomNumber) {
+        console.log('Меньше');
+    } else if (userGuess === randomNumber) {
+        console.log('Поздравляем! Вы угадали число.');
+    }
+
+} while (userGuess !== randomNumber);
+
