57b50f1e054020f7c24762f9d7552d73c779062b
Maksim Alipau <maxanotest@gmail.com>
2025-02-01 09:54:07 +0300

    feat: update a task 1 from Innodom

---
+++ hw2/task1.js
@@ -0,0 +1,11 @@
+// Напишите код, который просит пользователя ввести число number от 1 до 10 и выводит в консоль числа от 1 до number.
+
+const userNumber = +prompt('Введите число от 1 до 10:');
+
+if (userNumber >= 1 && userNumber <= 10) {
+    for (let i = 1; i <= userNumber; i++) {
+        console.log(i);
+    }
+} else {
+    console.log('Введено не корректное значение!');
+}
